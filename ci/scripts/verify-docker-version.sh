d34d4014beaf070f974c00ee67e907cc5ddd62b7
thaddeusa <thaddeusa@gmail.com>
2018-01-25 14:36:31 -0700

    add verify script and adjust pipeline with verify job

--- ci/scripts/verify-docker-version.sh
+++ ci/scripts/verify-docker-version.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/thaddeusa/docker-images.git
-cd docker-images
-git checkout version
-cp concourse-skinny-version ../version-git
+cp dockerversion/concourse-skinny-version version-git
 
 diff=$(diff version version-git)
 if [[ $? != 0 ]]; then
   echo "There is a difference."
   echo "Current version of this Docker image: $(cat version)"
   echo "Docker image version in github: $(cat version-git)"
+  exit 1
   ## take some significant action here!!
 else
   echo "no difference"
