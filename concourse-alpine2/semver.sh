88dc9a99dae290bdbf1687cff15a64944b7a3873
thaddeusa <thaddeusa@gmail.com>
2018-01-31 14:56:11 -0700

    correct version file name

--- concourse-alpine2/semver.sh
+++ concourse-alpine2/semver.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/thaddeusa/docker-images.git
 cd docker-images
 git checkout version
-cp concourse-skinny-version ../version
+cp concourse-alpine2-version ../version
 cd ..
 rm -rf docker-images
