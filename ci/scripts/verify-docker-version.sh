f5931cde49771ecb0fecb3a96415b8c6045aac6a
thaddeusa <thaddeusa@gmail.com>
2018-01-31 14:23:09 -0700

    remove exit during testing

--- ci/scripts/verify-docker-version.sh
+++ ci/scripts/verify-docker-version.sh
@@ -14,10 +14,11 @@ echo -e "yeah!"
 
 diff=$(diff version version-git)
 if [[ $? != 0 ]]; then
-  echo "There is a difference."
+  echo "Bad news: There is a difference."
   echo "Current version of this Docker image: $(cat version)"
   echo "Docker image version in github: $(cat version-git)"
-  exit 1
+  echo "Now I need to do something drastic..."
+#  exit 1
   ## take some significant action here!!
 else
   echo "no difference"
