9db7256f34148a674f2307e661da1b8bd7dbb88b
thaddeusa <thaddeusa@gmail.com>
2018-02-01 11:34:32 -0700

    add variables and update vars in pipeline and readme

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Docker Images
 
- - Dockerfile definitions, primarily for use in Concourse pipelines.
+ - Pipeline for building and testing docker images
+  - See README.md in `ci` folder
+ - Dockerfile definitions, primarily for use in Concourse pipelines
+
+### concourse-alpine
+  - Alpine 3.7-based Docker container for Concourse pipelines
+  - Lightweight base image with common linux tools and major cloud CLI's
+  - To use in Concourse pipeline:
+ ```
+   image_resource:
+     type: docker-image
+     source:
+       repository: cgicloudnative/concourse-alpine
+ ```
+
 
 ### concourse-ubuntu
  - Ubuntu Xenial-based Docker container for Concourse pipelines
--- concourse-alpine2/semver.sh
+++ concourse-alpine2/semver.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-git clone https://github.com/ECSTeam/docker-images.git
-cd docker-images
-git checkout version
-cp concourse-alpine2-version ../version
+# This script is copied to the docker image and ran as part of the build. It acquires the semver value written to a git repo during the build phase of the pipeline.
+
+# Modify the following variables based on your source git repo
+GIT_REPO=https://github.com/ECSTeam/docker-images.git
+GIT_REPO_FOLDER=docker-images
+GIT_BRANCH=version
+VERSION_FILE=concourse-alpine2-version
+
+git clone $GIT_REPO
+cd $GIT_REPO_FOLDER
+git checkout $GIT_BRANCH
+cp $VERSION_FILE ../version
 cd ..
-rm -rf docker-images
+rm -rf $GIT_REPO_FOLDER
